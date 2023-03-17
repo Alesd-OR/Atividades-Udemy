1104e8758edd83e7757bccbf249cbe15a786db37
Alessandro de Oliveira <Alessandro1395271@outlook.com>
2023-03-17 00:36:41 -0300

    Término da atividade e adicionado comentários

--- ProblemaAlturas.java
+++ ProblemaAlturas.java
@@ -10,38 +10,69 @@ public class ProblemaAlturas {
 
         Scanner oli = new Scanner(System.in);
         Locale.setDefault(Locale.US);
-        
 
+        // solicita ao usuário a quantidade de pessoas a serem cadastradas
         System.out.println("Quantas pessoas serão digitadas?");
         int qtdPessoas = oli.nextInt();
+
+        // cria arrays para armazenar nome, idade e altura das pessoas
         String[] dPessoas = new String[qtdPessoas];
         double[] dAltura = new double[qtdPessoas];
         int[] dIdade = new int[qtdPessoas];
+
+        // variável para armazenar a soma das alturas
         double soma = 0;
-        double porcentagemTotal = 100;
-        double pMenosde;
 
-        for(int i = 0; i < dPessoas.length; i++){
-        System.out.println("\nDados da " + (i+1) + " pessoa");
-        System.out.println("\nNome: ");
-        dPessoas[i] = oli.next();
+        // loop para obter informações de cada pessoa
+        for (int i = 0; i < dPessoas.length; i++) {
+            System.out.println("\nDados da " + (i + 1) + " pessoa");
+            System.out.println("\nNome: ");
+            dPessoas[i] = oli.next();
 
-        System.out.println("\nIdade: ");
-        dIdade[i] = oli.nextInt();
+            System.out.println("\nIdade: ");
+            dIdade[i] = oli.nextInt();
 
-        System.out.println("\nAltura: ");
-        dAltura[i] = oli.nextDouble();
-        soma += dAltura[i];
-    }
-    
+            System.out.println("\nAltura: ");
+            dAltura[i] = oli.nextDouble();
+
+            // adiciona a altura ao total para posterior cálculo da média
+            soma += dAltura[i];
+        }
+
         double media = soma / dAltura.length;
 
-        System.out.printf("ALTURA MÉDIA: %.2f", + media);
+        System.out.printf("ALTURA MÉDIA: %.2f\n", +media);
 
-        double porcentagem = (dIdade.length * pMenosde) / porcentagemTotal;
-        System.out.println("Pessoas com menos de 16 anos: " + porcentagem);
+        // variável para armazenar a quantidade de pessoas com idade menor que 16 anos
+        int countMenor16 = 0;
 
-    oli.close();
+        // array para armazenar o nome das pessoas com idade menor que 16 anos
+        String[] nomesMenor16 = new String[qtdPessoas];
+
+        // loop para percorrer o array de idades e contar quantas pessoas têm menos de
+        // 16 anos e armazenar os nomes delas
+        for (int i = 0; i < dIdade.length; i++) {
+            if (dIdade[i] < 16) {
+                nomesMenor16[countMenor16] = dPessoas[i];
+                countMenor16++;
+            }
+        }
+
+        // verifica se existem pessoas com menos de 16 anos cadastradas
+        if (countMenor16 > 0) {
+            // calcula a porcentagem de pessoas com menos de 16 anos
+            double porcentoMenor16 = (double) countMenor16 / qtdPessoas * 100;
+            // imprime a porcentagem formatada com duas casas decimais
+            System.out.printf("%.2f%% das pessoas têm menos de 16 anos. \n", porcentoMenor16);
+            // imprime os nomes das pessoas com menos de 16 anos
+            System.out.println("Nomes das pessoas com menos de 16 anos:");
+            for (int i = 0; i < countMenor16; i++) {
+                System.out.println(nomesMenor16[i]);
+            }
+        } else {
+            System.out.println("Não há pessoas com menos de 16 anos.");
+        }
+
+        oli.close();
     }
-    
 }
